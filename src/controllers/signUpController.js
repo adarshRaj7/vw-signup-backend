2875c1367c34fb82def3c1b7ae821fc979c22c37
Adarsh Raj <adarshrj62@gmail.com>
2024-04-14 15:55:54 +0530

    Added password encryption

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,6 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 const { Pool } = require("pg");
 const { sendEmail } = require("../helpers/resend");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 require("dotenv").config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -9,8 +11,13 @@ const pool = new Pool({
   },
 });
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   const { name, username, email, password } = req.body;
+  console.log("Before bcrypt", password);
+  const password_hash = await bcrypt.hash(password, saltRounds);
+  
+  console.log("After bcrypt", password_hash);
+
   let isEmailUnique = true;
   let isUsernameUnique = true;
   pool.query(
@@ -46,12 +53,13 @@ const createUser = (req, res) => {
       return;
     } else {
       pool.query(
-        "INSERT INTO users (name, email,username,password) VALUES ($1, $2, $3, $4) RETURNING id",
-        [name, email, username, password],
+        "INSERT INTO users (name, email,username,password_hash) VALUES ($1, $2, $3, $4) RETURNING id",
+        [name, email, username, password_hash],
         (error, results) => {
           if (error) {
             throw error;
           }
+          console.log(password_hash);
           res.status(201).json(results.rows[0]);
           isValid = false;
           return;
@@ -110,12 +118,12 @@ const addDetails = async (req, res) => {
   }
 };
 
-const addPreferences= (req,res)=>{
-  console.log(req.body)
-  const {id,interests}=req.body;
-  console.log(id,interests)
-  if(!id || !interests){
-    return res.status(400).json({error:"id and preferences are required"});
+const addPreferences = (req, res) => {
+  console.log(req.body);
+  const { id, interests } = req.body;
+  console.log(id, interests);
+  if (!id || !interests) {
+    return res.status(400).json({ error: "id and preferences are required" });
   }
   pool.query(
     `UPDATE users SET interests = $1 where id = $2`,
@@ -125,11 +133,13 @@ const addPreferences= (req,res)=>{
         console.error(error);
         return res.status(500).json({ error });
       } else {
-        await sendEmail(req.body.email,req.body.name,req.body.username);
-        res.status(200).json({ status: "success", message: "Preferences updated" });
+        await sendEmail(req.body.email, req.body.name, req.body.username);
+        res
+          .status(200)
+          .json({ status: "success", message: "Preferences updated" });
       }
     }
   );
-}
+};
 
-module.exports = { createUser, addDetails,addPreferences };
+module.exports = { createUser, addDetails, addPreferences };
